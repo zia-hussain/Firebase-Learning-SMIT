ed45aca4cb93439970e8b5f3253993250f6d9e06
zia-hussain <syedziashahgillani76@gmail.com>
2024-02-28 19:14:37 +0500

    TMDB setup done !

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,31 @@
 import "./App.css";
+import { useEffect } from "react";
+import { fetchDataFromApi } from "./Utils/api";
+import { useSelector, useDispatch } from "react-redux";
+import { getApiConfiguration } from "./store/homeSlice";
+
 function App() {
-  return <></>;
+  const dispatch = useDispatch();
+  const url = useSelector((state) => state.homeSlice.url); // Adjusted useSelector
+
+  useEffect(() => {
+    apiTesting();
+  }, []);
+
+  const apiTesting = () => {
+    fetchDataFromApi("/movie/popular").then((res) => {
+      dispatch(getApiConfiguration(res));
+    });
+  };
+
+  return (
+    <div className="App">
+      <h1>
+        App
+        {url}
+      </h1>
+    </div>
+  );
 }
 
 export default App;
