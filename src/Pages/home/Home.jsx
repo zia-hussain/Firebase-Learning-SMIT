ec9c784644d67ef067351cc7879ce099443c3735
zia-hussain <syedziashahgillani76@gmail.com>
2024-03-02 22:53:24 +0500

    Trending Section Starts Here !

--- src/Pages/home/Home.jsx
+++ src/Pages/home/Home.jsx
@@ -1,10 +1,13 @@
 import "./style.scss";
 import HeroBanner from "./heroBanner/HeroBanner";
+import Trending from "./trending/Trending";
 
 function Home() {
   return (
     <div>
       <HeroBanner />
+      <Trending />
+      <div style={{ height: 1000 }}></div>
     </div>
   );
 }
