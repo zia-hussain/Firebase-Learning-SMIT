655f374acab79bc261e9860d374c9fb68f72309c
zia-hussain <syedziashahgillani76@gmail.com>
2024-03-06 16:32:29 +0500

    Popular Section added !

--- src/Pages/home/Home.jsx
+++ src/Pages/home/Home.jsx
@@ -1,12 +1,14 @@
 import "./style.scss";
 import HeroBanner from "./heroBanner/HeroBanner";
 import Trending from "./trending/Trending";
+import Popular from "./popular/Popular";
 
 function Home() {
   return (
     <div>
       <HeroBanner />
       <Trending />
+      <Popular />
       <div style={{ height: 1000 }}></div>
     </div>
   );
