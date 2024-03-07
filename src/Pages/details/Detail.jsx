6ee35ec1027a050b29954d756123cf7c1be27e0b
zia-hussain <syedziashahgillani76@gmail.com>
2024-03-07 17:06:11 +0500

    Details Banner Complete !

--- src/Pages/details/Detail.jsx
+++ src/Pages/details/Detail.jsx
@@ -4,12 +4,15 @@ import "./style.scss";
 import { useParams } from "react-router-dom";
 import DetailsBanner from "./detailsBanner/DetailsBanner";
 function Detail() {
-  // const { mediaType, id } = useParams();
-  // const { data, loading } = useFetch(`/${mediaType}/${id}`);
-
+  const { mediaType, id } = useParams();
+  const { data, loading } = useFetch(`/${mediaType}/${id}/videos`);
+  const { data: credits, loading: creditsLoading } = useFetch(
+    `/${mediaType}/${id}/credits`
+  );
+  console.log(credits, "credits");
   return (
     <div>
-      <DetailsBanner />
+      <DetailsBanner video={data?.results?.[0]} crew={credits?.crew} />
     </div>
   );
 }
